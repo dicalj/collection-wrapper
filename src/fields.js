2f47fefe0c8a5c138a22099f74161038cef4be66
diego callejas <d.callejas@innlab.cl>
2020-08-12 18:57:41 -0400

    initial commit

---
+++ src/fields.js
@@ -0,0 +1,14 @@
+// required modules
+import Collection from './collection'
+
+/**
+ * This class describes a collection of fields used for forms or other situations that require input fields.
+ * @augments Collection
+ * @class Fields
+ */
+class Fields extends Collection {
+  
+}
+
+// export module
+export default Fields
---
+++ src/headers.js
@@ -0,0 +1,14 @@
+// required modules
+import Collection from './collection'
+
+/**
+ * This class describes a collection of headers used for tables or other situations that require columns headers.
+ * @augments Collection
+ * @class Headers
+ */
+class Headers extends Collection {
+  
+}
+
+// export module
+export default Headers
